a70d6e622b90719d236dfe98f43cc37ac2b3dcd5
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-30 11:59:47 -0400

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  api_key: 'PKRNRSRDOZ4TZDQWYA5H',
-  api_secret: 'PjkdVnYDVvKpjIKBvZ74TNsUa0vZ048P5tLR4qyk',
-  base_url: 'https://api.alpaca.markets',
-  paper_base_url: 'https://paper-api.alpaca.markets',
-  alpha_vantage_key: 'FX9TYVWCBE79345X',
-};
